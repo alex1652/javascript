a30b3c0476d42f7f17add33dde2ad2c07e9fc267
Alexis Deleon <alexisdeleon@Alexiss-MacBook-Pro.local>
2019-12-12 20:53:37 -0800

    add javascript numbers, strings, arrays, objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,50 @@
-var greeting = 'howdy' ;
-var name = 'moly';
-var welcomeMessage = greeting + name + '!';
-varl el = document.getElementById ('greeting') ;
-el .textContent = welcomeMessage;
+// Numbers
+// numbers and be computed with +, -, *, /, %, ++ increment, -- decrement
+// NaN means Not a Number
+var pizza = 5;
+var soda = 1;
+var bread = 3;
+
+var total = pizza + soda;
+
+console.log(total);
+
+/**
+ * The console. log() is a function in JavaScript which is used to print any
+   kind of variables defined before in it or to just print any message that 
+   needs to be displayed to the user. 
+ */
+
+ // String
+
+ const name ='Alexis Deleon';
+
+ const welcome = "Welcome to Geekwise Academy";
+
+
+ console.log(name);
+ console.log(welcome);
+
+ // Array
+ var fruits = [' apples' , 'grapes' , 'oranges' , 'bananas'];
+ var soccerTeams = ['juventus' , 'psg' , 'atletico' , 'inter'] ;
+ console.log(soccerTeams) ;
+
+ console.log(fruits) ; 
+
+ // Object
+
+ var car = {
+     type: "Fiat" ,
+     model: "500" ,
+     color: "white"
+ } ;
+
+ console.log(car) ;
+
+ // Boolean
+ // everythimg with a real value is true 0, null, undefined is false
+    
+ 
+
+ 
